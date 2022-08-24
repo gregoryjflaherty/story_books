607dab4086c655455b4e5d7ac9cfa272f9f56207
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-24 11:44:01 -0400

    Connect to port

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv')
 
 //Load config
-dotenv.config({ path: 'congif/config.env'})
+dotenv.config({ path: 'config/config.env'})
 
 const app = express()
-app.listen()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
