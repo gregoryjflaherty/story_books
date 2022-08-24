356d1b67e43f5a106568336eb846ff415d344ad9
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-24 12:13:24 -0400

    Add request logger and view template config

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 
 //Load config
 dotenv.config({ path: 'config/config.env'})
 
+const app = express()
+
 connectDB()
 
-const app = express()
+//handle bars
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
